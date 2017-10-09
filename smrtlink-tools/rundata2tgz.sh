fcd219ee788afbc414198201fdbb18b7612b7734
Stephane Plaisance <stephane.plaisance@vib.be>
2017-10-09 10:44:59 +0200

    edits

--- smrtlink-tools/rundata2tgz.sh
+++ smrtlink-tools/rundata2tgz.sh
@@ -15,24 +15,28 @@
 # visit our Git: https://github.com/Nucleomics-VIB
 
 # check parameters for your system
-version="1.1.1, 2017_09_20"
+version="1.1.2, 2017_10_09"
 usage='# Usage: rundata2tgz.sh
 # script version '${version}'
 ## input files
 # [required: -i <run-folder> (name of the run folder containing the flow-cell folder)]
 # [-f <flowcell name (default <1_A01> for a single-cell run)>]
-# [-o <output folder (default to <$GCDADA>]
+# [-o <output folder (default to <$GCDADA>)]
+# [-S <data root (default to <$SMRT_DATA>)]
 # [-l <show the list of runs currently present on the server>]
 # [-h for this help]'
 
 $( hash pigz 2>/dev/null ) || ( echo "# pigz not found in PATH"; exit 1 )
 
-while getopts "i:f:o:lh" opt; do
+while getopts "i:f:o:S:lh" opt; do
   case $opt in
     i) runfolder=${OPTARG} ;;
     f) flowcell=${OPTARG} ;;
     o) outpath=${OPTARG} ;;
-    l) ls $SMRT_DATA/r*; exit 0;;
+    l) echo "# Data currently in ${dataroot:-"$SMRT_DATA"}:"; 
+       ls ${dataroot:-"$SMRT_DATA"}/r*; 
+       exit 0 ;;
+    S) dataroot=${OPTARG} ;;
     h) echo "${usage}" >&2; exit 0 ;;
     \?) echo "Invalid option: -${OPTARG}" >&2; exit 1 ;;
     *) echo "this command requires arguments, try -h" >&2; exit 1 ;;
@@ -85,16 +89,19 @@ fi
 # check input defined
 testvariabledef "${runfolder}" "-i"
 
+# check data folder
+data_folder=${dataroot:-"$SMRT_DATA"}
+
 # check runfolder
 run_folder=${runfolder}
-testfolderexist "$SMRT_DATA/${run_folder}" "-i <run-folder-name (in $SMRT_DATA)>"
+testfolderexist "${data_folder}/${run_folder}" "-i <run-folder-name (in ${data_folder})>"
 
 # set to default if not provided
 flow_cell=${flowcell:-"1_A01"}
 
 # check flow-cell folder
 flowcell_path=${run_folder}/${flow_cell}
-testfolderexist "$SMRT_DATA/${flowcell_path}" "-i"
+testfolderexist "${data_folder}/${flowcell_path}" "-i"
 
 # check output folder
 archive_path=${outpath:-"$GCDATA"}
@@ -104,12 +111,12 @@ testfolderwritable "${archive_path}" "-o <output_folder>"
 archive_file=$(basename ${run_folder})-$(basename ${flowcell_path}).tgz
 
 # create archive (dereference/follow symlinks)
-echo "# creating archive from: ${run_folder}/${flow_cell}"
+echo "# creating archive from: ${data_folder}/${run_folder}/${flow_cell}"
 tar --use-compress-program="pigz" \
 	--exclude "*.h5" \
 	--exclude "*.baz" \
 	--exclude "*.log" \
-	-C $SMRT_DATA \
+	-C ${data_folder} \
 	-h -cvf \
 	${archive_path}/${archive_file} \
 	${run_folder}/${flow_cell}
@@ -117,9 +124,11 @@ tar --use-compress-program="pigz" \
 echo
 if [ $? -eq 0 ]; then
 	echo "# archive was created successfully, now checksumming"
-	md5sum ${archive_path}/${archive_file} | sed -r "s/ .*\/(.+)/  \1/g" > ${archive_path}/${archive_file}_md5.txt && \
-	echo; echo "# checksum saved as: ${archive_path}/${archive_file}_md5.txt" && \
-	du -a -h --max-depth=1 ${archive_path}/${archive_file}* | sort -hr ; cat ${archive_path}/${archive_file}_md5.txt
+	md5sum ${archive_path}/${archive_file} | sed -r "s/ .*\/(.+)/  \1/g" \
+		> ${archive_path}/${archive_file}_md5.txt && \
+		echo; echo "# checksum saved as: ${archive_path}/${archive_file}_md5.txt" && \
+		du -a -h --max-depth=1 ${archive_path}/${archive_file}* | \
+		sort -hr ; cat ${archive_path}/${archive_file}_md5.txt
 else
     echo "# something went wrong, please have a check!"
 fi
@@ -128,8 +137,9 @@ fi
 echo
 if [ $? -eq 0 ]; then
 	echo "# verifying the checksum against the archive"
-	md5sum -c ${archive_path}/${archive_file}_md5.txt 2>&1 | \
-	 tee -a ${archive_path}/${archive_file}_md5-test.txt
+	cd ${archive_path} && md5sum -c ${archive_file}_md5.txt 2>&1 | \
+	 tee -a ${archive_file}_md5-test.txt && \
+	 cd -
 else
     echo "# something went wrong, please have a check!"
 fi
