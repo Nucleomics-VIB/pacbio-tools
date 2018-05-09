2f32f81b5ec9ac44c27cd4bec3b01b83accc5c2f
Stephane Plaisance <stephane.plaisance@vib.be>
2018-05-09 16:59:54 +0200

    edits

--- smrtlink-tools/delete_SQ_data.sh
+++ smrtlink-tools/delete_SQ_data.sh
@@ -1,26 +1,43 @@
 #!/bin/bash
 
+# delete_SQ_data.sh
 # delete content of a Sequel run folder as the 'smrtanalysis' user
 # Stephane Plaisance (VIB-NC) 2018/01/26; v1.0
 # visit our Git: https://github.com/Nucleomics-VIB
+
+# check parameters for your system
+version="1.1, 2018_05_09"
+usage='# Usage: delete_SQ_data.sh
+# script version '${version}'
+## input files
+# [required: -f <run-folder> (runID/smartcell path to delete)]
+# [-r <data root (default to <$SMRT_DATA>)]
+# [-l <show the list of folders currently present on the server>]
+# [-h for this help]'
+
+# SMRTlink user
+pbuser="smrtanalysis"
+
+while getopts "f:r:lh" opt; do
+	case $opt in
+		f) foldername=${OPTARG} ;;
+		r) datapath=${OPTARG} ;;
+		l) echo "# Data currently in ${datapath:-"$SMRT_DATA"}:";
+			tree -I "000" -L 3 ${datapath:-"$SMRT_DATA"}; 
+			exit 0 ;;
+		h) echo "${usage}" >&2; exit 0 ;;
+		\?) echo "Invalid option: -${OPTARG}" >&2; exit 1 ;;
+		*) echo "this command requires arguments, try -h" >&2; exit 1 ;;
+	esac
+done
 
 if [ $# -lt 1 ]; then
   echo "# please provide the name of the Sequel run folder to empty"
   exit 0
 fi
 
-pbuser="smrtanalysis"
-datapath=/data/pacbio/sequel
-foldername=$1
 folderpath=${datapath}/${foldername}
 date=$(date +%Y%m%d)
-
-# list data if -l provided
-if [[ $1 == "-l" ]]
-then
-  tree -I "000" -L 3 ${datapath}
-  exit 0
-fi
 
 echo "Preparing to delete the content of ${folderpath}"
 
@@ -33,7 +50,7 @@ then
     su -c "rm -rf ${folderpath}/* && touch ${folderpath}/deleted_${date}" ${pbuser}
   else
     echo "# folder not found in the Sequel data path:"
-    ls ${datapath}
+    tree -I "000" -L 2 ${datapath}
     exit 1
   fi
 else
