c3bd2f0348352b7d2db1df3eea674971ca6fef18
Stephane Plaisance <stephane.plaisance@vib.be>
2018-08-02 10:47:39 +0200

    edits

--- bam-utils/SamKeepReadNames.pl
+++ bam-utils/SamKeepReadNames.pl
@@ -7,20 +7,26 @@ use warnings;
 $|=1;
 
 # script: SamKeepReadNames.pl
-# parse a SAM file and a list of polymerase names (excluding coordinates)
-# save matching reads to stdout
-# REM: can be used to extract control reads from a subread file for downstream CCS analysis
+# parse a SAM file and a text file with a list of read names (first field of a SAM record)
+# print matching rows to stdout
+#
+# requires samtools to parse SAM/BAM
+#
+# Stéphane Plaisance - VIB-NC 2018_08_03, v1.01
+#
+# visit our Git: https://github.com/Nucleomics-VIB
 
-my $usage = "Usage $0 <input.sam> <FOFN>\n";
+my $usage = "Usage $0 <SAM/BAM> <list_of_read-names.txt>\n";
 my $infile = shift;
-my $fofn = shift or die $usage;
+my $lorn = shift or die $usage;
 
 # store read names in array
-open (LIST, $fofn) || die "cannot open file ".$fofn."!";
+open (LIST, $lorn) || die "cannot open file ".$lorn."!";
 my @list;
 my $count;
 my $match;
 
+# load list in RAM
 while (my $id = <LIST>) {
 	$count++;
 	chomp($id);
@@ -28,13 +34,16 @@ while (my $id = <LIST>) {
 	}
 close LIST;
 
-print STDERR "$count read names loaded\n";
+# keep unique records only
+my @unique = do { my %seen; grep { !$seen{$_}++ } @list };
+
+print STDERR scalar(@unique)." unique read names loaded\n";
 
 # parse and filter SAM
 open (BAM, "samtools view -F 256 -h $infile | ") || die "cannot open file ".$infile." with samtools!";
 
 while(<BAM>){
-	## keep header lines (if you used -h in the samools command)
+	## keep header lines as we used -h in the samtools command
 	if(/^(\@)/) {
 		print STDOUT $_;
 		next;
@@ -42,10 +51,8 @@ while(<BAM>){
 	
 	# parse one row
 	my $name = (split(/\t+/))[0];
-	#print STDOUT $name."\n";
-	my $poly = $name =~ s/\/[^\/]*$//r;
-	#print STDOUT $poly."\n";
-	if ( grep( /^$poly$/, @list ) ) {
+
+	if ( grep( /^$name$/, @unique ) ) {
   		print STDOUT $_;
   		$match++;
   		$match =~ /100$/ && print STDERR "+";
