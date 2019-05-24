35d103cb6e9de47b7cb1a3e345397d0633e8f3f6
stephane plaisance <stephane.plaisance@vib.be>
2019-05-24 12:26:58 +0200

    edits

--- plotting-tools/Isoseq3_QC-plots.R
+++ plotting-tools/Isoseq3_QC-plots.R
@@ -2,17 +2,18 @@
 
 # script: Isoseq3_QC-plots.R
 # create plots from Isoseq3 polished.cluster_report.csv
-# create plot from Isoseq3 unpolished.cluster
 # designed for single smartcell data !
-# SP & Joke A. 2019-05-21 v2.1
+# SP & Joke A. 2019-05-24 v3
 
 # required libraries
 suppressMessages(library("readr"))
 suppressMessages(library("dplyr"))
 suppressMessages(library("stringr"))
 suppressMessages(library("ggplot2"))
+suppressMessages(library("scales"))
 suppressMessages(library("grid"))
 suppressMessages(library("gridExtra"))
+suppressMessages(library("data.table"))
 
 args <- commandArgs(trailingOnly=TRUE)
 # argument for plot title
@@ -22,6 +23,10 @@ if (length(args)==0) {
 } else if (length(args)==1) {
   title <- args[1]
 }
+
+####################################################################################
+# load polished.cluster_report.csv
+####################################################################################
 
 polished_cluster_report <- suppressMessages(read_csv("polished.cluster_report.csv"))
 
@@ -52,61 +57,56 @@ p1 <- suppressMessages(ggplot(hist.data[hist.data$n<=lim,], aes(n)) +
   xlab("number of supporting CCS reads") +
   ylab("Transcript count"))
 
-# 10x subset 0 to 100% and count unique Transcripts
-plot.data <- data.frame(CCS_sample=0, Transcript_count=0)
+# 10x subset 0 to 100% & count unique Transcripts
+# remove transcripts with only one row & count unique Transcripts
+plot.data <- data.frame(CCS_sample=numeric(), Transcript_count=numeric(), min=factor())
+
 for (iter in seq(1, 10, by=1)) {
-for (i in seq(0.1, 1, by=0.1)) {
+for (i in seq(0, 1, by=0.1)) {
+  # sample and count
   dat <- sample_frac(polished_cluster_report, i)
-  plot.data <- rbind(plot.data, c( i, length(unique(dat$cluster_id))) )
-}
+  plot.data <- rbind(plot.data, c( i, length(unique(dat$cluster_id)), 0) )
+  # group and add counts
+  dat <- dat %>%
+    group_by(cluster_id) %>%
+    summarise(n = n()) 
+  # filter at FLNC=1 and count
+  res <- dat %>%
+    filter(n == 1)
+  plot.data <- rbind(plot.data, c( i, length(unique(res$cluster_id)), 1) )
+  # filter at FLNC>1 and count
+  res <- dat %>%
+    filter(n > 1)
+  plot.data <- rbind(plot.data, c( i, length(unique(res$cluster_id)), 2) )
+  # filter at FLNC>2 and count
+  res <- dat %>%
+    filter(n > 2)
+  plot.data <- rbind(plot.data, c( i, length(unique(res$cluster_id)), 3) )
+  }
 }
 
-p2 <- suppressMessages(ggplot(plot.data, aes(x=CCS_sample, y=Transcript_count)) +
-  scale_x_continuous(labels = scales::percent) +
+# merge the two dataframes and plot
+colnames(plot.data) <- c("CCS_sample", "Transcript_count", "min")
+
+p2 <- suppressMessages(ggplot(plot.data, aes(x=CCS_sample, y=Transcript_count, group=min, shape==factor(min), colour=factor(min))) +
   geom_smooth(method="loess", se=FALSE, fullrange=TRUE, size=1) +
-  geom_point(shape=20, size=4, color="red") +
+  geom_point(size=3) +
+  scale_shape_identity() +
+  scale_x_continuous(labels = scales::percent) +
   theme_classic() +
   theme(axis.text.x=element_text(size=rel(1)),
         axis.text.y=element_text(size=rel(1)),
         text = element_text(size=12)) +
-  xlab("CCS sample") +
-  ylab("Transcript count (10 random pulls)"))
-
-# load and process unpolished.cluster
-pairwise <- suppressMessages(read_table2("unpolished.cluster"))
-# simplify content (ony works when data comes from a single Smart-cell)
-pairwise$from <- str_replace(pairwise$from, "(.*)_(.*)_(.*)/(.*)/ccs","\\4")
-pairwise$to <- str_replace(pairwise$to, "(.*)_(.*)_(.*)/(.*)/ccs","\\4")
-
-# 10x subset 0 to 100% and count n>1
-saturation.data <- data.frame(FLNC_sample=numeric(), Cluster_count=numeric())
-
-for (iter in seq(1, 10, by=1)) {
-for (sub in seq(0, 1, by=0.1)) {
-  data <- sample_frac(pairwise, sub) %>%
-    group_by(to) %>%
-    summarise(n = n()) %>%
-    filter(n > 1) 
-  saturation.data <- rbind(saturation.data, c(sub, nrow(data)))
-}
-}
-
-colnames(saturation.data) <- c("FLNC_sample", "Cluster_count")
-
-p3 <- ggplot(saturation.data, aes(x=FLNC_sample, y=Cluster_count)) +
-  scale_x_continuous(labels = scales::percent) +
-  geom_smooth(method="loess", se=FALSE, fullrange=TRUE, size=1) +
-  geom_point(shape=20, size=4, color="red") +
-  theme_classic() +
-  theme(axis.text.x=element_text(size=rel(1)),
-        axis.text.y=element_text(size=rel(1)),
-        text = element_text(size=12)) +
-  xlab("FLNC sample") +
-  ylab("Cluster count (10 random pulls)")
+  xlab("CCS sample from polished.cluster_report.csv") +
+  ylab("Transcript count (10 random pulls)") +
+  scale_color_manual(labels = c("one or more", "exactly 1", "more than 1", "more than 2"), 
+                       values = hue_pal()(4)) +
+  guides(color=guide_legend("CCS count \n/Transcript"))
+)
 
 pdf("isoseq3_QC-plots.pdf", onefile = TRUE, width=12, height=8)
-lay <- rbind(c(1,1,1,1,5,2,2,2),
-             c(3,3,3,3,4,4,4,4))
+lay <- rbind(c(1,1,1,1,4,2,2,2),
+             c(3,3,3,3,3,4,4,4))
 table.legend <- "X% transcripts have Y or less supporting CCS's\n\n\n"
 mytheme <- gridExtra::ttheme_default(
   core = list(fg_params=list(cex = 0.65)),
@@ -117,7 +117,6 @@ grid.arrange(p1,
                        rows=NULL, 
                        theme = mytheme), 
              p2, 
-             p3,
              ncol=2,
              top=textGrob(title, gp=gpar(fontsize=20, font=3)),
              layout_matrix = lay,
