0d0ee6b1f4164a013dcb06b5d22d6ff44f58d16f
Stephane Plaisance <stephane.plaisance@vib.be>
2020-11-24 11:34:05 +0100

    Update ccsbamfilter.sh

--- bam-utils/ccsbamfilter.sh
+++ bam-utils/ccsbamfilter.sh
@@ -2,7 +2,8 @@
 
 # ccsbamfilter.sh: 
 # filter a PB CCS BAM file
-# output results as a new bam# filter by:
+# output results as a new bam of a fastq (bgzipped)
+# filter by:
 # - min and max insert length
 # - min Accuracy
 # - min ccs pass number
@@ -14,6 +15,7 @@
 # required:
 # Samtools for parsing bam
 # GNU awk for parsing sam data
+# bgzip for fastq compression
 
 version="1.0, 2020_11_24"
 
@@ -22,15 +24,17 @@ usage='# Usage: ccsbamfilter.sh -i <bam file>
 # [optional: -l <min length |0>]
 # [optional: -L <max length |1000000>]
 # [optional: -a <min accuracy|0>]
-# [optional: -p <min pass# |1>]'
+# [optional: -p <min pass# |1>]
+# [optional: -f <output fastq instead of bam>]'
 
-while getopts "i:l:L:a:p:h" opt; do
+while getopts "i:l:L:a:p:fh" opt; do
   case $opt in
     i) bamfile=${OPTARG} ;;
     l) optminl=${OPTARG} ;;
     L) optmaxl=${OPTARG} ;;
     a) optmina=${OPTARG} ;;
     p) optminp=${OPTARG} ;;
+    f) optfrmt="fastq" ;;
     h) echo "${usage}" >&2; exit 0 ;;
     \?) echo "Invalid option: -${OPTARG}" >&2; exit 1 ;;
     *) echo "this command requires arguments, try -h" >&2; exit 1 ;;
@@ -50,15 +54,22 @@ minlen=${optminl:-0}
 maxlen=${optmaxl:-1000000}
 minacc=${optmina:-0}
 minpass=${optminp:-1}
-
-outfile=$(basename ${bamfile%.bam})_minl-${minlen}_maxl-${maxlen}_mina-${minacc}_minpass-${minpass}.bam
+outfmt=${optfrmt:-"bam"}
+outpfx=$(basename ${bamfile%.bam})_minl-${minlen}_maxl-${maxlen}_mina-${minacc}_minpass-${minpass}
 
 # check if requirements are present
 $( hash samtools 2>/dev/null ) || ( echo "# samtools not found in PATH"; exit 1 )
 $( hash awk 2>/dev/null ) || ( echo "# awk not found in PATH"; exit 1 )
+$( hash bgzip 2>/dev/null ) || ( echo "# bgzip not found in PATH"; exit 1 )
 
+if [ "${outfmt}" == "bam" ]; then
+
+# make it a bam
 samtools view -h ${bamfile} | \
-awk -v minl="${minlen}" -v maxl="${maxlen}" -v mina="${minacc}" -v minp="${minpass}" \
+awk -v minl="${minlen}" \
+-v maxl="${maxlen}" \
+-v mina="${minacc}" \
+-v minp="${minpass}" \
 'BEGIN{FS="\t"; OFS="\t"}
 {if ($1 ~ /^@/) print $0;
 else if ($14 ~ /np:i/){
@@ -67,4 +78,23 @@ if (len >= minl && len <= maxl && rq[3] >= mina && np[3] >= minp){
 print $0
 }
 }
-}' | samtools view -Sb > ${outfile}
+}' | samtools view -Sb > "${outpfx}.bam"
+
+else
+
+# make it a fastq.gz
+samtools view -h ${bamfile} | \
+awk -v minl="${minlen}" \
+-v maxl="${maxlen}" \
+-v mina="${minacc}" \
+-v minp="${minpass}" \
+'BEGIN{FS="\t"; OFS="\t"}
+{if ($14 ~ /np:i/){
+len=length($10); split($14,np,":"); split($15,rq,":");
+if (len >= minl && len <= maxl && rq[3] >= mina && np[3] >= minp){
+print "@"$1" len:"len" accuracy:"rq[3]" passes:"np[3]"\n"$10"\n+\n"$11
+}
+}
+}' | bgzip -c > "${outpfx}.fq.gz"
+
+fi
