48bf42de8946cd7dfc9c07602bc2b4b035ebabb8
Stephane Plaisance <stephane.plaisance@vib.be>
2022-06-24 10:53:09 +0200

    new for CCS v10+

---
+++ bam-utils/bam2sizedist_ccs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# bam2sizedist_ccs.sh: 
+# parse a PB CCS BAM file (PB v11)
+# https://pacbiofileformats.readthedocs.io/en/11.0/BAM.html
+# extract molecule ID, read length, barcode information, and polymerase coordinates
+# save results to a text table (TSV) for stats in R
+#
+# Stéphane Plaisance - VIB-NC-BITS Jan-31-2017 v1.2
+#
+# visit our Git: https://github.com/Nucleomics-VIB
+
+# required:
+# Samtools for parsing bam
+# GNU awk for parsing sam data
+
+if [ -z "${1}" ]
+then
+	echo "# provide a bam file to be parsed!"
+	exit 1
+else
+	inbam=$1
+fi
+
+# check if requirements are present
+$( hash samtools 2>/dev/null ) || ( echo "# samtools not found in PATH"; exit 1 )
+$( hash awk 2>/dev/null ) || ( echo "# awk not found in PATH"; exit 1 )
+
+# read_name: m54094_180411_025859/4260449/16464_22394
+#                 {movieName}/{holeNumber}/{qStart}_{qEnd}
+# h[1] = movieName = deviceID_yymmdd_hhmmss (m54094_180411_025859)
+# h[2] = ZMWID (4260449)
+# h[3] = qS_qE (16464_22394)
+
+samtools view ${inbam} | \
+awk 'BEGIN{FS="\t"; OFS=","; print "Mol.ID","start","end","FBC","RBC","BCQ","len"}
+	{split($1,hd,"/");
+	split(hd[3],co,"_");
+	if( $28 ~ /bc:B:S/ ){
+		split($28,bc,":"); 
+		split(bc[3],id,","); 
+		split($29,q,":"); 
+		print hd[2],co[1],co[2],id[2],id[3],q[3],length($10) 
+		} else {
+			print hd[2],co[1],co[2],"na","na","na",length($10)
+			}
+	}' > $(basename ${inbam%%.bam})_length-dist.txt
