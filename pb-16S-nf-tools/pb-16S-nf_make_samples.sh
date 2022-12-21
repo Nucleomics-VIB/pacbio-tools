c0bf8725565d6274a75344ea148448f010d482ea
Stephane Plaisance <stephane.plaisance@vib.be>
2022-12-21 13:48:36 +0100

    edits

--- pb-16S-nf-tools/pb-16S-nf_make_samples.sh
+++ pb-16S-nf-tools/pb-16S-nf_make_samples.sh
@@ -7,6 +7,9 @@ echo -e "sample-id\tabsolute-file-path" > run_samples.tsv
 # find all fastq and add rows to tsv
 readfolder=${1:-fastq_results}
 
+# read files are named like
+#  m64279e_221217_093107.hifi_reads.bc1024--bc1044.fastq.gz
+
 for fq in $(find ${readfolder} -name "*.fastq.gz" -exec readlink -f {} \;);
 do bc=$(basename ${fq} | cut -d "." -f 3)
 echo -e "${bc}\t${fq}";
