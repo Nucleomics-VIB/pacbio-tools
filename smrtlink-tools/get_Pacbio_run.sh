06bc7d63ea0408c35c3d76431f5f2100a65e3ea7
Stephane Plaisance <stephane.plaisance@vib.be>
2025-06-18 10:09:19 +0200

    Update get_Pacbio_run.sh

--- smrtlink-tools/get_Pacbio_run.sh
+++ smrtlink-tools/get_Pacbio_run.sh
@@ -7,49 +7,40 @@
 #
 # visit our Git: https://github.com/Nucleomics-VIB
 
-version="2.0, 2022-05-12"
+version="2.1, 2022-06-18"
 
-usage='# Usage: get_pacbio_run.sh <args>
-# -R <runs_dir (obtained from "gsutil ls gs://gcpi-rvvnc/")>
-# -r <run_id (obtained from "gsutil ls gs://gcpi-rvvnc/<runs_dir>")>
-# -l <show the list of runs_dir currently present on the server>]
+usage='# Usage: get_Pacbio_run.sh <args>
+# -R <runs_dir (default: "runs")>
+# -r <run_id (obtained from "-l" or "gsutil ls gs://gcpi-rvvnc/<runs_dir>")>
+# -l <show the current list of runs_dir on the server>]
 # -h <this help>
 # script version '${version}'
 # [-h for this help]'
+
+# Set default value for rundir
+rundir="runs"
 
 while getopts "R:r:lh" opt; do
   case $opt in
     R) rundir=${OPTARG} ;;
     r) runid=${OPTARG} ;;
     l) echo "# Runs data currently available on the bucket";
-    		gsutil ls gs://gcpi-rvvnc/${rundir:-""}/${runid:-""};
-			exit 0 ;;
+       gsutil ls gs://gcpi-rvvnc/${rundir}/${runid:-""};
+       exit 0 ;;
     h) echo "${usage}" >&2; exit 0 ;;
     \?) echo "Invalid option: -${OPTARG}" >&2; exit 1 ;;
     *) echo "this command requires arguments, try -h" >&2; exit 1 ;;
   esac
 done
 
-# test if minimal arguments were provided
-if [ -z "${rundir}" ]
-then
-   echo "# no run folder name provided!"
-   echo "${usage}"
-   exit 1
+# Transfer mode requires runid
+if [ -n "${runid}" ]; then
+  # create local folder
+  mkdir -p ${runid} && cd ${runid}
+
+  # get run folder
+  echo -e "\n# getting run data"
+  gsutil -m rsync -r "gs://gcpi-rvvnc/${rundir}/${runid}/" .
+
+  echo -e "\n\n# copy done"
 fi
-
-if [ -z "${runid}" ]
-then
-   echo "# no runID folder name provided!"
-   echo "${usage}"
-   exit 1
-fi
-
-# create local folder
-mkdir -p ${rundir}/${runid} && cd ${rundir}/${runid}
-
-# get run folder
-echo -e "\n# getting run data"
-gsutil -m rsync -r "gs://gcpi-rvvnc/${rundir}/${runid}/" .
-
-echo -e "\n\n# copy done"
